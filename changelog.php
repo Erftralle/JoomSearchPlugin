a8977d04c44b02f63ec0bdc1cff4dd2cc1311635
Erftralle <Erftralle@users.noreply.github.com>
2019-04-23 22:38:20 +0200

    Prepare for 3.1.1 release
    Co-Authored-By: Alwin <team@joomgalleryfriends.net>

---
+++ changelog.php
@@ -0,0 +1,24 @@
+<?php
+defined('_JEXEC') or die('Restricted access');
+?>
+
+CHANGELOG plg_search_joomgallery (since Version 3.1.1)
+
+Legende / Legend:
+
+* -> Security Fix
+# -> Bug Fix
++ -> Addition
+^ -> Change
+- -> Removed
+! -> Note
+
+
+-------------------------------------------------------------------------------
+PLG_SEARCH_JOOMGALLERY version: 3.1.1
+-------------------------------------------------------------------------------
+20190403
+^ Change update server
+
+-------------------------------------------------------------------------------
+
